30291308cde52741ff78907fd17caa132608586f
Eirik Robertstad <eirik.robertstad@rallyhealth.com>
2016-07-22 15:05:55 -0400

    added updateFilter support - DEVOPS-2645

--- resources/mirror.rb
+++ resources/mirror.rb
@@ -17,7 +17,7 @@
 # limitations under the License.
 #
 
-actions :create, :update, :drop
+actions :create, :update, :drop, :updateFilter
 default_action :create if defined?(default_action)
 
 # Needed for Chef versions < 0.10.10
